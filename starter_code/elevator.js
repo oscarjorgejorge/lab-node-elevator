05cf914cd2bcbce85b311fcb2d57c3aeadfdc76f
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-01-30 09:12:32 +0100

    lost

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,43 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction;
+    this.waitingList = [];
+    this.passengers = [];
+    this.requests = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() { 
+    const intervalId = setInterval(function (){
+       update();
+    }, 1000)
+  }
+  stop() {
+    clearInterval(intervalId);
+   }
+  update() { 
+    log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp() { 
+    if (this.MAXFLOOR < 10) {
+      this.floor ++;
+    }
+  }
+  floorDown() { 
+    if (this.floor > 0) {
+      this.floor --;
+    }
+  }
+  call(Person) { 
+    const newPerson = new Person ('Juan', 0, 3);
+    this.waitingList.push(newPerson);
+    this.requests.push(newPerson.originFloor);
+  }
+  log() { 
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
+  }
 }
 
 module.exports = Elevator;
--- starter_code/person.js
+++ starter_code/person.js
@@ -1,6 +1,13 @@
 class Person {
   constructor(name, originFloor, destinationFloor){
+    this.name = name;
+    this.originFloor = originFloor;
+    this.destinationFloor = destinationFloor;
   }
 }
+
+const juan = new Person('Juan', 0, 3);
+
+
 
 module.exports = Person;
